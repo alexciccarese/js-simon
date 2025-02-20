2b7bb8a36e63b0cbc8566371cd60619597c6aef4
alexciccarese <alex.ciccarese@gmail.com>
2025-02-20 16:14:03 +0100

    add timout to hide number

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,23 @@
 const randomEl = document.querySelectorAll('.random-number')
-const inputEl = document.querySelector('.input-numb')
+const inputEl = document.querySelectorAll('.input-numb')
 const buttonEl = document.querySelector('.btn')
 
 function generateRandom() {
   for (let i = 0; i < randomEl.length; i++) {
-    let randomNumber = Math.floor(Math.random() * 100) + 1
+    const randomNumber = Math.floor(Math.random() * 100) + 1
     randomEl[i].innerText = randomNumber
   }
-
+  
+  setTimeout (() => {
+    for (let i = 0; i < randomEl.length; i++) {
+      const randomNumber = Math.floor(Math.random() * 100) + 1
+      randomEl[i].style.display = 'none'
+    }
+  }, 3000);
 }
-generateRandom()
+
+
+const randomNumber = generateRandom()
+
+
 
